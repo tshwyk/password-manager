285ee548b54f1fe6095338f0a4c7f8738f4fb89a
Petr Tshwyk <tshwyk@gmail.com>
2019-02-19 02:03:34 +0100

    Refactoring

--- App/Utils.cpp
+++ App/Utils.cpp
@@ -14,9 +14,9 @@
 extern sgx_enclave_id_t enclave_id;
 
 typedef struct _sgx_errlist_t {
-  sgx_status_t err;
-  const char *msg;
-  const char *sug; /* Suggestion */
+    sgx_status_t err;
+    const char *msg;
+    const char *sug; /* Suggestion */
 } sgx_errlist_t;
 
 /* Error code returned by sgx_create_enclave */
@@ -43,93 +43,84 @@ static sgx_errlist_t sgx_errlist[] = {
 
 /* Check error conditions for loading enclave */
 void print_error_message(sgx_status_t ret) {
-  size_t idx = 0;
-  size_t ttl = sizeof sgx_errlist / sizeof sgx_errlist[0];
+    size_t idx = 0;
+    size_t ttl = sizeof sgx_errlist / sizeof sgx_errlist[0];
 
-  for (idx = 0; idx < ttl; idx++) {
-    if (ret == sgx_errlist[idx].err) {
-      if (NULL != sgx_errlist[idx].sug)
-        printf("Info: %s\n", sgx_errlist[idx].sug);
-      printf("Error: %s\n", sgx_errlist[idx].msg);
-      break;
+    for (idx = 0; idx < ttl; idx++) {
+        if (ret == sgx_errlist[idx].err) {
+            if (sgx_errlist[idx].sug != NULL)
+                printf("Info: %s\n", sgx_errlist[idx].sug);
+            printf("Error: %s\n", sgx_errlist[idx].msg);
+            break;
+        }
     }
-  }
 
-  if (idx == ttl)
-    printf(
-        "Error code is 0x%X. Please refer to the \"Intel SGX SDK Developer "
-        "Reference\" for more details.\n",
-        ret);
+    if (idx == ttl)
+        printf("Error code is 0x%X. Please refer to the \"Intel SGX SDK "
+            "Developer Reference\" for more details.\n", ret);
 }
 
-/* Initialize the enclave:
- *   Step 1: try to retrieve the launch token saved by last transaction
- *   Step 2: call sgx_create_enclave to initialize an enclave instance
- *   Step 3: save the launch token if it is updated
+/*
+ *  Initialize the enclave:
+ *      Step 1: try to retrieve the launch token saved by last transaction
+ *      Step 2: call sgx_create_enclave to initialize an enclave instance
+ *      Step 3: save the launch token if it is updated
  */
 int initialize_enclave(void) {
-  char token_path[MAX_PATH] = {'\0'};
-  sgx_launch_token_t token = {0};
-  sgx_status_t ret = SGX_ERROR_UNEXPECTED;
-  int updated = 0;
+    sgx_launch_token_t token = {0};
+    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+    int updated = 0;
 
-  /* Step 1: try to retrieve the launch token saved by last transaction
-   *         if there is no token, then create a new one.
-   */
-  /* try to get the token saved in $HOME */
-  const char *home_dir = getpwuid(getuid())->pw_dir;
+    /*
+    *    Step 1: try to retrieve the launch token saved by last transaction
+    *        if there is no token, then create a new one.
+    */
+    FILE *fp = fopen(TOKEN_FILENAME, "rb");
+    if (fp == NULL && (fp = fopen(TOKEN_FILENAME, "wb")) == NULL)
+        printf("Warning: Failed to create/open the launch token file \"%s\".\n",
+           TOKEN_FILENAME);
 
-  if (home_dir != NULL &&
-      (strlen(home_dir) + strlen("/") + sizeof(TOKEN_FILENAME) + 1) <=
-          MAX_PATH) {
-    /* compose the token path */
-    strncpy(token_path, home_dir, strlen(home_dir));
-    strncat(token_path, "/", strlen("/"));
-    strncat(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME) + 1);
-  } else {
-    /* if token path is too long or $HOME is NULL */
-    strncpy(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME));
-  }
-
-  FILE *fp = fopen(token_path, "rb");
-  if (fp == NULL && (fp = fopen(token_path, "wb")) == NULL) {
-    printf("Warning: Failed to create/open the launch token file \"%s\".\n",
-           token_path);
-  }
-
-  if (fp != NULL) {
-    /* read the token from saved file */
-    size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
-    if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
-      /* if token is invalid, clear the buffer */
-      memset(&token, 0x0, sizeof(sgx_launch_token_t));
-      printf("Warning: Invalid launch token read from \"%s\".\n", token_path);
+    if (fp != NULL) {
+        /* read the token from saved file */
+        size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
+        if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
+            /* if token is invalid, clear the buffer */
+            memset(&token, 0x0, sizeof(sgx_launch_token_t));
+            printf("Warning: Invalid launch token read from \"%s\".\n",
+                TOKEN_FILENAME);
+        }
     }
-  }
-  /* Step 2: call sgx_create_enclave to initialize an enclave instance */
-  /* Debug Support: set 2nd parameter to 1 */
-  ret = sgx_create_enclave(ENCLAVE_FILENAME, SGX_DEBUG_FLAG, &token, &updated,
+    /*
+     *  Step 2: call sgx_create_enclave to initialize an enclave instance
+     *      Debug Support: set 2nd parameter to 1
+     */
+    ret = sgx_create_enclave(ENCLAVE_FILENAME, SGX_DEBUG_FLAG, &token, &updated,
                            &enclave_id, NULL);
-  if (ret != SGX_SUCCESS) {
-    print_error_message(ret);
-    if (fp != NULL) fclose(fp);
-    return -1;
-  }
+    if (ret != SGX_SUCCESS) {
+        print_error_message(ret);
+        if (fp != NULL) fclose(fp);
+        return -1;
+    }
 
-  /* Step 3: save the launch token if it is updated */
-  if (updated == 0 || fp == NULL) {
-    /* if the token is not updated, or file handler is invalid, do not perform
-     * saving */
-    if (fp != NULL) fclose(fp);
+    /*
+     *  Step 3: save the launch token if it is updated
+     */
+    if (updated == 0 || fp == NULL) {
+        /*
+         *  if the token is not updated, or file handler is invalid, do not
+         *      perform saving
+         */
+        if (fp != NULL) fclose(fp);
+        return 0;
+    }
+
+    /* reopen the file with write capablity */
+    fp = freopen(TOKEN_FILENAME, "wb", fp);
+    if (fp == NULL) return 0;
+    size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
+    if (write_num != sizeof(sgx_launch_token_t))
+        printf("Warning: Failed to save launch token to \"%s\".\n",
+            TOKEN_FILENAME);
+    fclose(fp);
     return 0;
-  }
-
-  /* reopen the file with write capablity */
-  fp = freopen(token_path, "wb", fp);
-  if (fp == NULL) return 0;
-  size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
-  if (write_num != sizeof(sgx_launch_token_t))
-    printf("Warning: Failed to save launch token to \"%s\".\n", token_path);
-  fclose(fp);
-  return 0;
 }
--- Enclave/Enclave.h
+++ Enclave/Enclave.h
@@ -6,22 +6,15 @@ extern "C" {
 #endif
 
 sgx_status_t new_vault(char *passphrase);
-
 sgx_status_t vault_size(uint32_t *vault_size);
-
 sgx_status_t save_vault(uint8_t *vault_data, uint32_t vault_size);
-
 sgx_status_t load_vault(uint8_t *vault_data, uint32_t vault_size,
-                        char *passphrase);
+    char *passphrase);
 
 sgx_status_t create_account(char *name, char *user, char *pass);
-
 sgx_status_t remove_account(char *name);
-
 sgx_status_t has_account(char *name);
-
 sgx_status_t get_username(char *name, char *user, uint32_t size);
-
 sgx_status_t get_password(char *name, char *pass, uint32_t size);
 
 #if defined(__cplusplus)
